520a96bcd475142381abd09816e47d149dfc1585
harvey <harvey@teaches.cc>
2020-10-20 13:38:04 +0800

    push build

---
+++ build/precache-manifest.4d8f334f41499d73918f9bb723535950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3c0f1754d777ef417d0c29f78446f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed7a94ff55a44aad6991",
+    "url": "/static/css/2.17d3fd7e.chunk.css"
+  },
+  {
+    "revision": "6010f347410b87a35091",
+    "url": "/static/css/main.f4c55db1.chunk.css"
+  },
+  {
+    "revision": "ed7a94ff55a44aad6991",
+    "url": "/static/js/2.1d88ba98.chunk.js"
+  },
+  {
+    "revision": "f2a3140c7700b1c8edefe12ce2e38979",
+    "url": "/static/js/2.1d88ba98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6010f347410b87a35091",
+    "url": "/static/js/main.a387528a.chunk.js"
+  },
+  {
+    "revision": "83fd8490856823ff1596",
+    "url": "/static/js/runtime-main.0777fb7b.js"
+  }
+]);
